09e6b907a0780901067bfdc474533ab499fdddbc
alhamodi65 <91828081+alhamodi65@users.noreply.github.com>
2022-04-22 07:13:51 +0200

    init

---
+++ android/app/src/main/kotlin/com/example/otp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.otp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.arrow_back_ios_new),
+            color: Colors.black),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.info),
+              color: Colors.black),
+        ],
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 40,
+          ),
+          const Icon(Icons.dialpad_rounded, size: 50),
+          const SizedBox(
+            height: 40,
+          ),
+          const Text(
+            "Enter Mary's PIN",
+            style: TextStyle(fontSize: 40),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const [
+              Otp(),
+              Otp(),
+              Otp(),
+              Otp(),
+            ],
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          const Text(
+            "Rider can't find a pin",
+            style: TextStyle(fontSize: 20),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Otp extends StatelessWidget {
+  const Otp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 50,
+      height: 50,
+      child: TextFormField(
+        keyboardType: TextInputType.number,
+        style: Theme.of(context).textTheme.headline6,
+        textAlign: TextAlign.center,
+        inputFormatters: [
+          LengthLimitingTextInputFormatter(1),
+          FilteringTextInputFormatter.digitsOnly
+        ],
+        onChanged: (value) {
+          if (value.length == 1) {
+            FocusScope.of(context).nextFocus();
+          }
+          if (value.isEmpty) {
+            FocusScope.of(context).previousFocus();
+          }
+        },
+        decoration: const InputDecoration(
+          hintText: ('0'),
+        ),
+        onSaved: (value) {},
+      ),
+    );
+  }
+}
